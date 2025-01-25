3ce5b5469beda2108a2b67ed408012e09be409ed
Your Name <you@example.com>
2025-01-25 20:57:38 +0300

    дз_водитель

---
+++ 6-driver/dz.js
@@ -0,0 +1,9 @@
+const hasLicence = false;
+const age = 18;
+const isDrunk = false;
+
+if (age >=18 && hasLicence === true && !isDrunk === true){
+    console.log('Может');
+} else {
+    console.log('Не может');
+}
