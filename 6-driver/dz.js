2b977f6c9e66ce16fc90c7c9630cd786e792c5fe
6rkdtui <151437205+6rkdtui@users.noreply.github.com>
2025-02-05 15:17:15 +0300

    Delete 6-driver directory

--- 6-driver/dz.js
+++
@@ -1,9 +0,0 @@
-const hasLicence = false;
-const age = 18;
-const isDrunk = false;
-
-if (age >=18 && hasLicence === true && !isDrunk === true){
-    console.log('Может');
-} else {
-    console.log('Не может');
-}
